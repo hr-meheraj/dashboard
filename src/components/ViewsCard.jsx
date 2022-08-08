75420442dcecc9e69bc708a61e4400fc4959941c
hr-meheraj <hrmeheraj2021@gmail.com>
2022-08-09 02:53:59 +0600

    CArd finished first section

--- src/components/ViewsCard.jsx
+++ src/components/ViewsCard.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 function ViewsCard({imgURL, titleOne, titleTwo, views, bids}) {
     return (
-        <div className='grid grid-cols-12 gap-5 mx-auto w-[95%]'>
-            <div className="col-span-7 flex gap-3">
+        <div className='grid grid-cols-12 gap-5 mx-auto w-[95%] shadow-md rounded-[6px] p-4 content-center'>
+            <div className="col-span-7 flex gap-3 items-center">
                 <img className=' rounded-[4px] w-[56px] h-[44px]' src={imgURL} alt={titleOne}/>
                 <p className='text-[#0d0a19] text-[14px] font-normal'>{titleOne} <br /> {titleTwo}</p>
             </div>
 
-            <div className="col-span-5 grid gap-4 grid-cols-2">
+            <div className="col-span-5 grid gap-4 grid-cols-2 items-center">
                 <div className="flex flex-col items-center gap-2">
                     <h3 className='text-[14px] font-bold text-[#0D0A19]'>{views}</h3>
                     <p className=' text-[#9A9EA7] text-[12px]'>Views</p>
                 </div>
-                <div className="flex flex-col items-center gap-2">
+                <div className="flex flex-col items-center gap-2 items-center">
                     <h3 className='text-[14px] font-bold text-[#0D0A19]'>{bids}</h3>
                     <p className=' text-[#9A9EA7] text-[12px]'>Last Bid</p>
                 </div>
